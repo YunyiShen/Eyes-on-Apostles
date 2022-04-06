67851a75fd07ccf5a8d8841f42d71d99959310b0
YunyiShen <yshen99@wisc.edu>
2022-04-05 19:54:20 -0500

    add some functionals

--- app.R
+++ app.R
@@ -8,16 +8,20 @@ library(geojsonio)
 library(jsonlite)
 library(forecast) # to forcast detection rate
 library(dplyr)
+library(knitr)
+library(reshape2)
+aaa <- lapply(list.files("helpers","R$", full.names = TRUE), source)
+
 
 # read necessary data
-APIS_bound <- rgdal::readOGR("maps/APIS_boundaries.geojson")
-presence_map <- read.csv("data/PA_all_full.csv",row.names = 1)[-c(150:155),]
+APIS_bound <- rgdal::readOGR("maps/APIS_boundaries.geojson") # this is the polygon for island
+presence_map <- read.csv("data/PA_all_full.csv",row.names = 1)[-c(150:155),] # PA matrix
 spp_list <- colnames(presence_map)
-camera_loci <- read.csv("data/CT_loci.csv")[-c(150:155),-c(6,7)]
-camera_loci$richness <- rowSums(presence_map)
+camera_loci <- read.csv("data/CT_loci.csv")[-c(150:155),-c(6,7)] # camera locations
+camera_loci$richness <- rowSums(presence_map) # richness
 camera_loci$spplist<- sapply(camera_loci$name, function(site, sppmat){
   paste(colnames(sppmat)[sppmat[site,]!=0], collapse = " <br/> ")
-}, as.matrix(presence_map))
+}, as.matrix(presence_map)) # for popup species list on map
 APIS_map <- leaflet(APIS_bound) %>%
   addPolygons(label = ~paste(TRACT_ID,"island")) 
 
@@ -25,6 +29,7 @@ APIS_map <- leaflet(APIS_bound) %>%
 ui <- dashboardPage(
   skin = "red",
   dashboardHeader(title = "Eyes on Apostles Data Dashboard"),
+  ### sidebar ### 
   dashboardSidebar(
     sidebarMenu(
       menuItem("Map", tabName = "dashboard", icon = icon("map")),
@@ -35,25 +40,50 @@ ui <- dashboardPage(
         )
     ),
     radioButtons("bubble","Map showing", choices = list("Richness" = TRUE,"Presence-absence" = FALSE)),
-    checkboxInput("tiles","Base map", value = TRUE),
     selectInput("species", "Choose a species to show:",
                 choices = colnames(presence_map), selected = "Bear_black"),
     selectInput("species2", "Choose a species to compare in analysis:",
                 choices = colnames(presence_map), selected = "Deer"),
-    radioButtons("comparing","Compare:", choices = list("True" = TRUE,"False" = FALSE))
-    
+    radioButtons("comparing","Compare:", choices = list("False" = FALSE,"True" = TRUE)),
+    sidebarMenu(
+      menuItem("About", tabName = "about", icon = icon("address-card"))
+    )
   ),
-  
+  ### end sidebar ###
+  ### body ###
   dashboardBody(
-    tabItems( # dashboard tab
+    ### dashboard tab ###
+    tabItems( 
+      ### map tab ###
       tabItem(tabName = "dashboard",
         h3("Camera sites"),
         fluidRow(
             leafletOutput("apismap")
         )
-      ),# end dashboard tab
-      tabItem(tabName = "RAItrend"),
-      tabItem(tabName = "Temporalniche")
+      ),
+      ### end map tab ###
+      ### RAI tab ###
+      tabItem(tabName = "RAItrend",
+              h3("Detection trend"),
+              box(plotOutput("raitrend"),title = "RAI"),
+              box(plotOutput("ndet"), title = "number of detections")
+              
+              ),
+      ### temporal tab ###
+      tabItem(tabName = "Temporalniche",
+              h3("Activity pattern"),
+              box(plotOutput("annualtemp"), title = "Annual"),
+              box(plotOutput("dailytemp"), title = "Daily")
+              ),
+      ### end temporal tab ###
+      ### about tab ###
+      tabItem(tabName = "about",
+              fluidPage(
+                uiOutput("about", inline = T)
+              )
+              )
+      ### end about tab ###
+      
     )
   )
 )
@@ -66,9 +96,9 @@ server <- function(input, output) {
       species <- input$species
     pal <- colorFactor(c("navy", "darkred"), domain = c(TRUE, FALSE))
     # whether to add base map
-    if(input$tiles){
-      APIS_map <- APIS_map %>% addTiles()
-    }
+    
+      APIS_map <- APIS_map %>% addTiles(group = "OSM (default)") %>%
+        addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite")
     
     camera_loci$PA_spp <- (presence_map[,species] == 1)
     PA <- SpatialPointsDataFrame( camera_loci[,c("lon","lat")], camera_loci) 
@@ -79,16 +109,52 @@ server <- function(input, output) {
       addCircleMarkers(data = PA, radius = ~richness/1.5,
                       color = "darkred",
                       popup = ~as.character(spplist), label = ~paste(name,"richness:",richness)
-        ) 
+        ) %>% 
+      addLayersControl(
+        baseGroups = c("OSM (default)", "Toner Lite"),
+        options = layersControlOptions(collapsed = TRUE)
+      )
     }  else {
       APIS_map <- 
         APIS_map %>%
         addCircleMarkers(data = PA, radius = 1.5,
                          color = ~pal(PA_spp),
                          popup = ~as.character(spplist), label = ~paste(name,input$species," presence:", PA_spp)
-        ) 
+        ) %>% 
+        addLegend(pal = pal, values = ~PA$PA_spp, opacity = 1, 
+                  title = paste(species, "presence")
+        ) %>% 
+        addLayersControl(
+          baseGroups = c("OSM (default)", "Toner Lite"),
+          options = layersControlOptions(collapsed = TRUE)
+        )
     }
   })
+######## RAI #########
+    
+    
+    output$raitrend <- renderPlot({
+      get_RAI(input$species, input$comparing, input$species2)
+    })
+######## number of detections #########
+    
+    output$ndet <- renderPlot({
+      get_count(input$species, input$comparing, input$species2)
+    })
+######## temporal niche #########
+    output$annualtemp <- renderPlot({
+      get_annual_temp(input$species, input$comparing, input$species2)
+    })
+    
+    output$dailytemp <- renderPlot({
+      hist(rnorm(100), main = "place holder")
+    })
+
+######## about #########
+    output$about <- renderUI({
+      includeMarkdown('docs/about.Rmd') 
+    })
+    
 }
 
 shinyApp(ui, server)
---
+++ helpers/count_det.R
@@ -0,0 +1,135 @@
+get_raw_counts <- function(sppdata){
+  date_spp1 <- as.POSIXct(sppdata$Date, format = "%m/%d/%Y", tz = "CDT")
+  years_spp1 <- as.numeric(format(date_spp1, "%Y"))
+  summary(as.factor(years_spp1)) 
+}
+
+get_count <- function(spp1, compare, spp2){
+  rawdata <- read.csv("data/CleanEvents.csv")
+  rawdata <- rawdata[ rawdata$Date!="",]
+  date_all <- as.POSIXlt(rawdata$Date, format = "%m/%d/%Y", tz = "CDT")
+  years <- as.numeric(format(date_all, "%Y"))
+  julian_date <- as.numeric(format(date_all, "%j"))
+  
+  camera_days <- sapply(sort(unique(years)), function(year, years, julian_date){
+    c( year,length(unique(julian_date[years==year])))
+  }, years, julian_date) %>% t() %>% data.frame()
+  colnames(camera_days) <- c("year", "camera_days")
+  
+  spp_1_data <- rawdata[rawdata$Species==spp1,]
+  if(compare) spp_2_data <- rawdata[rawdata$Species==spp2,]
+  rm(rawdata)
+  rm(date_all, years, julian_date)
+  n_detections1 <- get_raw_counts(spp_1_data)
+  camera_days$spp1 <- n_detections1[as.character(camera_days$year)]
+  #camera_days$spp1rai <- n_detections1[as.character(camera_days$year)]/camera_days$camera_days * 100
+  
+  #plots <- list()
+  
+  
+  
+  if(compare){
+    n_detections2 <- get_raw_counts(spp_2_data)
+    camera_days$spp2 <- n_detections2[as.character(camera_days$year)]
+    #camera_days$spp2rai <- n_detections2[as.character(camera_days$year)]/camera_days$camera_days * 100
+    
+    plot_data_rawcount <- melt(camera_days[,c(1,3,4)], id.vars = "year")
+    spp_raw <- c("spp1"=spp1, "spp2"=spp2)
+    
+    plot_data_rawcount$species <- spp_raw[plot_data_rawcount$variable]
+    ggplot(plot_data_rawcount, aes(x = year, y = value, col = species)) + 
+      geom_smooth() + 
+      geom_point() + 
+      ylab("Raw counts")
+    
+  }else{
+    
+    ggplot(data = camera_days, aes(x = year, y = spp1)) + 
+      geom_smooth() +
+      geom_point() + 
+      ylab(paste(spp1, "raw detection counts"))
+  }
+}
+
+get_RAI <- function(spp1, compare, spp2){
+  rawdata <- read.csv("data/CleanEvents.csv")
+  rawdata <- rawdata[ rawdata$Date!="",]
+  date_all <- as.POSIXlt(rawdata$Date, format = "%m/%d/%Y", tz = "CDT")
+  years <- as.numeric(format(date_all, "%Y"))
+  julian_date <- as.numeric(format(date_all, "%j"))
+  
+  camera_days <- sapply(sort(unique(years)), function(year, years, julian_date){
+    c( year,length(unique(julian_date[years==year])))
+  }, years, julian_date) %>% t() %>% data.frame()
+  colnames(camera_days) <- c("year", "camera_days")
+  
+  spp_1_data <- rawdata[rawdata$Species==spp1,]
+  if(compare) spp_2_data <- rawdata[rawdata$Species==spp2,]
+  rm(rawdata)
+  rm(date_all, years, julian_date)
+  n_detections1 <- get_raw_counts(spp_1_data)
+  #camera_days$spp1 <- n_detections1[as.character(camera_days$year)]
+  camera_days$spp1rai <- n_detections1[as.character(camera_days$year)]/camera_days$camera_days * 100
+  
+  
+  if(compare){
+    n_detections2 <- get_raw_counts(spp_2_data)
+    #camera_days$spp2 <- n_detections2[as.character(camera_days$year)]
+    camera_days$spp2rai <- n_detections2[as.character(camera_days$year)]/camera_days$camera_days * 100
+    
+    plot_data_rai <- melt(camera_days[,c(1,3,4)], id.vars = "year")
+    spp_rai <- c("spp1rai"=spp1, "spp2rai"=spp2)
+    
+    plot_data_rai$species <- spp_rai[plot_data_rai$variable]
+    
+    ggplot(plot_data_rai, aes(x = year, y = value, col = species)) + 
+      geom_smooth() + 
+      geom_point() + 
+      ylab("RAI")
+    
+  }else{
+    ggplot(data = camera_days, aes(x = year, y = spp1rai)) + 
+      geom_smooth() + 
+      geom_point() + 
+      ylab(paste(spp1, "RAI"))
+    
+  }
+}
+
+
+
+get_annual_temp <- function(spp1, compare, spp2){
+  rawdata <- read.csv("data/CleanEvents.csv")
+  rawdata <- rawdata[ rawdata$Date!="",]
+  
+  
+  spp_1_data <- rawdata[rawdata$Species==spp1,]
+  if(compare) spp_2_data <- rawdata[rawdata$Species==spp2,]
+  rm(rawdata)
+  date_spp1 <- as.POSIXct(spp_1_data$Date, format = "%m/%d/%Y", tz = "CDT")
+  dates_spp1 <- as.numeric(format(date_spp1, "%j"))
+  
+  density_data <- data.frame(species = spp1, juliandate = dates_spp1)
+   
+  
+  if(compare){
+    date_spp2 <- as.POSIXct(spp_2_data$Date, format = "%m/%d/%Y", tz = "CDT")
+    dates_spp2 <- as.numeric(format(date_spp2, "%j"))
+    density_data2 <- data.frame(species = spp2, juliandate = dates_spp2)
+    density_data <- rbind(density_data,density_data2)
+    
+    ggplot(density_data, aes(x = juliandate, col = species)) + 
+      geom_density() + 
+      xlab("Julian date")
+    
+  }else{
+    ggplot(density_data, aes(x = juliandate)) + 
+      geom_density() + 
+      xlab("Julian date")
+    
+  }
+}
+
+
+
+
